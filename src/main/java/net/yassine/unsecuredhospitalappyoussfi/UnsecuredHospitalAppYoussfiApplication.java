24e5d783de0391be9ca6588bbb7e76c81db114bb
yassine4real <yssnabbou2002@gmail.com>
2024-12-11 10:35:03 +0100

    instanstiate object and add to mysql-db

--- src/main/java/net/yassine/unsecuredhospitalappyoussfi/UnsecuredHospitalAppYoussfiApplication.java
+++ src/main/java/net/yassine/unsecuredhospitalappyoussfi/UnsecuredHospitalAppYoussfiApplication.java
@@ -1,14 +1,31 @@
 package net.yassine.unsecuredhospitalappyoussfi;
 
+
+import net.yassine.unsecuredhospitalappyoussfi.entities.Patient;
+import net.yassine.unsecuredhospitalappyoussfi.repository.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
 
 @SpringBootApplication
 public class UnsecuredHospitalAppYoussfiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UnsecuredHospitalAppYoussfiApplication.class, args);
-        System.out.println("yassine");
+    }
+
+    @Bean
+    CommandLineRunner start(PatientRepository patientRepository){
+        return args -> {
+            patientRepository.save(new Patient(null,"Mohamed",new Date(),false,42));
+            patientRepository.save(new Patient(null,"Imane",new Date(),true,98));
+            patientRepository.save(new Patient(null,"Yassine",new Date(),true,342));
+            patientRepository.save(new Patient(null,"Laila",new Date(),false,123));
+        };
     }
 
 }
