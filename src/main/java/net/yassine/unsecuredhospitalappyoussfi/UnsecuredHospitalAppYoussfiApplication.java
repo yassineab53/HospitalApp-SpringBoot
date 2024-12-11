4d1b7dad551c3a8aa5cc45ab9b86735d3de0bec4
yassine4real <yssnabbou2002@gmail.com>
2024-12-11 09:58:11 +0100

    first commit

---
+++ src/main/java/net/yassine/unsecuredhospitalappyoussfi/UnsecuredHospitalAppYoussfiApplication.java
@@ -0,0 +1,14 @@
+package net.yassine.unsecuredhospitalappyoussfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsecuredHospitalAppYoussfiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnsecuredHospitalAppYoussfiApplication.class, args);
+        System.out.println("yassine");
+    }
+
+}
---
+++ src/test/java/net/yassine/unsecuredhospitalappyoussfi/UnsecuredHospitalAppYoussfiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yassine.unsecuredhospitalappyoussfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsecuredHospitalAppYoussfiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
